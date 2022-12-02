784e4cc6a7456a5e7a27d40e2eb7e836bfe846f8
Tareq35 <a.t.tariqulislam@gmail.com>
2022-12-02 21:02:02 +0600

    banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,34 +2,18 @@ import React from 'react';
 import './Banner.css';
 
 const Banner = () => {
-    return (
-      <div className="banner lg:flex items-center justify-center p-4 text-white">
-        <div className="banner-section">
-          <div>
-            <img
-              className=" lg:w-[350px] h-[430px] banner-img"
-              src="https://img.freepik.com/free-photo/picture-elegant-young-fashion-man_158595-546.jpg?w=826&t=st=1668061907~exp=1668062507~hmac=893037c799311eff15e2226ba186a79b74833d800ee6cf667c169cef6baa7c93"
-              alt=""
-            />
-          </div>
-          <div className="">
-            <p className=" lg:text-7xl font-mono font-semibold">We Enforce</p>
-            <h1 className=" text-6xl font-serif font-semibold">
-              Justice For You
-            </h1>
-            <h3 className="lg:text-lg  uppercase text-center text-black font-medium bg-[#D9A86D] py-2 my-3 rounded-lg">
-              I Am Always Prepared For Any Case{" "}
-            </h3>
-            <p className="">
-              A lawyer is a professional who is qualified to offer advice about
-              the law or represent someone in legal matters. A lawyer can also
-              be called an attorney, a solicitor, a counselor, a barrister, or —
-              pejoratively — an ambulance chaser.
-            </p>
-          </div>
+  return (
+    <div className="banner items-center justify-center p-4 text-white">
+      <div className="banner-section text-center w-1/2 mx-auto mt-20">
+        <div>
+          <p className=" lg:text-7xl font-mono font-semibold">We are here to help you <br /> to <br /> remember the best days.</p>
+          <p className="font-mono mt-5">
+          Our in-house photography services team made up of professional photographers can add value to your website with high-resolution team photos, corporate event photography and product photography.
+          </p>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Banner;

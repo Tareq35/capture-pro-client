824ea9076c3bce21067fde65acf856fab7113092
Tareq35 <a.t.tariqulislam@gmail.com>
2022-12-02 20:25:20 +0600

    header updated

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import toast from 'react-hot-toast';
+import logo from '../../../logo/logo.png'
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../Contexts/AuthProvider/AuthProvider';
 
@@ -13,7 +14,119 @@ const Header = () => {
   }
 
     return (
-      <div className="navbar bg-base-100 rounded-sm shadow-2xl mb-3  top-0">
+      <div className="navbar bg-sky-700 rounded-md shadow-2xl mb-10 top-0">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+
+              {user?.uid ? (
+                <>
+                  <li>
+                    <Link to="/myReview" className=" font-bold">
+                      My Reviews
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/addService" className=" font-bold">
+                      Add Service
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={handleLogout} className=" font-bold">
+                      Logout
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <li>
+                  <Link to="/login" className=" font-bold">
+                    Login
+                  </Link>
+                </li>
+              )}
+              <li>
+                <Link to="/blog" className=" font-bold">
+                  Blog
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <Link to="/" className=" flex items-center">
+            <div>
+              <img
+                className=" w-12 mr-2"
+                src={logo}
+                alt=""
+              />
+            </div>
+            <div>
+              <p className=" font-serif normal-case text-2xl tracking-widest font-bold">
+                Capture Pro
+              </p>
+            </div>
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal p-0 text-lg">
+            <li>
+              <Link to="/" className=" font-bold">
+                Home
+              </Link>
+            </li>
+            {user?.uid ? (
+              <>
+                <li>
+                  <Link to="/myReview" className=" font-bold">
+                    My Reviews
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/addService" className=" font-bold">
+                    Add Service
+                  </Link>
+                </li>
+                <li>
+                  <Link onClick={handleLogout} className=" font-bold">
+                    Logout
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <li>
+                <Link to="/login" className=" font-bold">
+                  Login
+                </Link>
+              </li>
+            )}
+            <li>
+              <Link to="/blog" className=" font-bold">
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     );
 };
